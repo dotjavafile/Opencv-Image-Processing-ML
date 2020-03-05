288ecf2e82b0ea7f47ad18c03b4cf00e3ad6a430
paolo.albiero <paolo.albiero@gmail.com>
2020-03-05 13:53:20 +0100

    *minor fix at line 10

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 i= result =1.00
-j =c= 0
+j =a= 0
 while i > 0:
     i = float(input(""))
     if i>=0 :
-        c+=i
+        a+=i
         j+=1
     else:
         break
-result=c/j
+result=a/j
 print(str(round(result,2)))
 
 
